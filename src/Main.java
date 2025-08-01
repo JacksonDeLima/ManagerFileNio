726806d48082e68e0a9109a60686a2f715c3712b
JacksonDeLima <jacksonluis444@gmail.com>
2025-08-01 17:13:59 -0300

    add escrita commit inicial

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import br.com.dio.persistence.FilePersistence;
+import br.com.dio.persistence.NIOFilePersistence;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        FilePersistence persistence = new NIOFilePersistence("user.csv");
+        System.out.println(persistence.write("bianca;bia@bia.com;22/09/1997;"));
+        System.out.println("========================");
+        System.out.println(persistence.write("bernardo; bernardo@bernardo.com;28/11/19999;"));
+        System.out.println("========================");
+        System.out.println(persistence.write("Ricardo;ricardo@ricardo.com;12/01/2000;"));
+        System.out.println("========================");
+
+
+    }
+}
